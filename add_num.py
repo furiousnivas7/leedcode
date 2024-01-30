af1026c3c65903c6a32bb6a66bb407f851766c4e
Nivash_sharma <nivasmathu@gmail.com>
2024-01-30 15:43:27 +0530

    suceesful run but still doubt  in leedcode environment

--- add_num.py
+++ add_num.py
@@ -6,13 +6,13 @@ list3 = [int(n) for n in list1.split(",") if n]
 list4 = [int(n) for n in list2.split(",") if n]
 
 y = ''  # Initialize y as an empty string
-for i in range(len(list3) - 1, -1, -1):
+for i in range(len(list3) - 1,-1 , -1):
     x = str(list3[i])
     y = y + x
 z = int(y)
 
 y = ''  # Reset y for the second number
-for i in range(len(list4) - 1, -1, -1):
+for i in range(len(list4) - 1,-1, -1):
     x = str(list4[i])
     y = y + x
 r = int(y)
